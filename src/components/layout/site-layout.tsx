2d93b96b3bbd0692b045ffd3c3498e5ca7b08f21
LAGOStaiko <lagos@lagostaiko.net>
2026-02-01 09:51:55 +0900

    fix: add more spacing between header and content

--- src/components/layout/site-layout.tsx
+++ src/components/layout/site-layout.tsx
@@ -8,7 +8,7 @@ export function SiteLayout() {
       <div className='relative'>
         <SiteHeader />
 
-        <main className='flex w-full flex-1 bg-black pt-16 pb-16 md:pb-20'>
+        <main className='flex w-full flex-1 bg-black pt-20 pb-16 md:pt-24 md:pb-20'>
           <SiteContainer className='w-full'>
             <Outlet />
           </SiteContainer>
